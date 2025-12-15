b99946b19293963572c9fd89403fceb5b5731487
Ipaidforwinrar1 <pangws05@gmail.com>
2025-12-14 20:52:51 -0500

    Initial commit

---
+++ src/client/java/com/yourname/stellarrage/StellarRageClient.java
@@ -0,0 +1,10 @@
+package com.yourname.stellarrage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StellarRageClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
